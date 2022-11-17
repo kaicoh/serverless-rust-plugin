ea04154468ba578a10759038ab305bc7f731dbde
Kanji Tanaka <ktanaka@ichain.co.jp>
2022-11-18 08:00:51 +0900

    Add src for integration test

---
+++ tests/integration/src/main.rs
@@ -0,0 +1,16 @@
+use lambda_runtime::{service_fn, LambdaEvent, Error};
+use serde_json::{json, Value};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let func = service_fn(func);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
+    let (event, _context) = event.into_parts();
+    let first_name = event["firstName"].as_str().unwrap_or("world");
+
+    Ok(json!({ "message": format!("Hello, {}!", first_name) }))
+}
