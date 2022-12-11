dc4fc4542bc81d62c1aba4272cb69cdc787c0d94
Kanji Tanaka <ktanaka@ichain.co.jp>
2022-12-11 20:21:55 +0900

    Add unit tests

--- tests/lib/proxy/route.test.js
+++ tests/lib/proxy/route.test.js
@@ -1,5 +1,13 @@
+const { Readable, Writable, Transform } = require('stream');
+const http = require('http');
 const Route = require('../../../lib/proxy/route');
 const { pathRegexp } = require('../../../lib/proxy/route');
+const ApiGatewayProxyRequest = require('../../../lib/proxy/request');
+const ApiGatewayProxyResponse = require('../../../lib/proxy/response');
+
+jest.mock('http');
+jest.mock('../../../lib/proxy/request');
+jest.mock('../../../lib/proxy/response');
 
 describe('pathRegexp', () => {
   it('converts to path string to regexp', () => {
@@ -31,6 +39,10 @@ describe('Route', () => {
       request: { parameters },
     };
   }
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
 
   describe('method: match', () => {
     it('returns true if the given request matches the path', () => {
@@ -406,6 +418,261 @@ describe('Route', () => {
         headers: { host: 'localhost' },
       };
       expect(route.getQueryParams(req)).toEqual({});
+    });
+  });
+
+  describe('displayLines', () => {
+    const config = {
+      funcName: 'test function',
+      method: 'test method',
+      path: 'hello',
+    };
+
+    it('returns an array from strings including function name', () => {
+      const route = new Route(config);
+      expect(route.displayLines()[0]).toEqual('func: test function');
+    });
+
+    it('returns an array from strings including method and path', () => {
+      const route = new Route(config);
+      expect(route.displayLines()[1]).toEqual('  TEST METHOD /hello');
+    });
+
+    describe('when header parameters exist', () => {
+      let result;
+
+      beforeEach(() => {
+        const route = new Route({
+          ...config,
+          request: {
+            parameters: {
+              querystrings: {
+                foo: true,
+                bar: false,
+              },
+            },
+          },
+        });
+
+        result = route.displayLines();
+      });
+
+      it('returns an array from strings including query parameters', () => {
+        expect(result[2]).toEqual('    query parameters');
+      });
+
+      it('sets as required and optional from the configuration', () => {
+        expect(result).toEqual(expect.arrayContaining([
+          '      foo: required',
+          '      bar: optional',
+        ]));
+      });
+    });
+
+    describe('when query parameters exist', () => {
+      let result;
+
+      beforeEach(() => {
+        const route = new Route({
+          ...config,
+          request: {
+            parameters: {
+              headers: {
+                foo: true,
+                bar: false,
+              },
+            },
+          },
+        });
+
+        result = route.displayLines();
+      });
+
+      it('returns an array from strings including query parameters', () => {
+        expect(result[2]).toEqual('    header parameters');
+      });
+
+      it('sets as required and optional from the configuration', () => {
+        expect(result).toEqual(expect.arrayContaining([
+          '      foo: required',
+          '      bar: optional',
+        ]));
+      });
+    });
+  });
+
+  describe('proxy', () => {
+    let req;
+    let res;
+    let body;
+
+    beforeEach(() => {
+      body = '';
+      req = Readable.from('request from local');
+      res = new Writable({
+        write(chunk, _, cb) {
+          body += chunk.toString();
+          cb();
+        },
+      });
+      res.writeHead = jest.fn();
+    });
+
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    describe('if the request results in validation error', () => {
+      let mock;
+
+      beforeEach(() => {
+        const route = new Route({ path: 'hello' });
+        route.validate = jest.fn(() => ['some error']);
+
+        mock = jest.spyOn(http, 'request');
+
+        route.proxy(req, res, () => {});
+      });
+
+      afterEach(() => {
+        mock.mockRestore();
+      });
+
+      it('returns validation error response', () => {
+        expect(res.writeHead).toHaveBeenCalledWith(400, { 'content-type': 'application/json' });
+        expect(body).toEqual('{"errors":["some error"]}');
+      });
+
+      it('doesn\'t call http.request', () => {
+        expect(http.request).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('if the request doesn\'t have validation error', () => {
+      let reqToLambda;
+      let mock;
+      let route;
+
+      beforeEach(() => {
+        reqToLambda = '';
+
+        route = new Route({ path: 'hello', port: 8888 });
+        route.validate = jest.fn(() => []);
+
+        mock = jest.spyOn(http, 'request').mockImplementation((_, cb) => {
+          const reqToLambdaStream = new Writable({
+            write(chunk, __, done) {
+              reqToLambda += chunk.toString();
+              done();
+            },
+          });
+
+          req.on('end', () => {
+            cb(Readable.from('response from lambda'));
+          });
+
+          return reqToLambdaStream;
+        });
+
+        ApiGatewayProxyRequest.mockClear();
+        ApiGatewayProxyRequest.mockImplementation(() => new Transform({
+          transform(chunk, _, done) {
+            this.push(chunk);
+            done();
+          },
+          flush(done) {
+            this.push(' transformed from api gateway proxy request stream');
+            done();
+          },
+        }));
+
+        ApiGatewayProxyResponse.mockClear();
+        ApiGatewayProxyResponse.mockImplementation(() => new Transform({
+          transform(chunk, _, done) {
+            this.push(chunk);
+            done();
+          },
+          flush(done) {
+            this.push(' transformed from api gateway proxy response stream');
+            done();
+          },
+        }));
+      });
+
+      afterEach(() => {
+        mock.mockRestore();
+      });
+
+      it('calls http.request with options from the route port', (done) => {
+        route.proxy(req, res, () => {});
+        res.on('close', () => {
+          expect(http.request).toHaveBeenCalledWith(
+            {
+              hostname: 'localhost',
+              port: 8888,
+              path: '/2015-03-31/functions/function/invocations',
+              method: 'POST',
+            },
+            expect.anything(),
+          );
+
+          done();
+        });
+      });
+
+      it('passes a transformed request to lambda', (done) => {
+        route.proxy(req, res, () => {});
+        res.on('close', () => {
+          expect(reqToLambda).toEqual('request from local transformed from api gateway proxy request stream');
+          done();
+        });
+      });
+
+      it('passed a transformed response to local', (done) => {
+        route.proxy(req, res, () => {});
+        res.on('close', () => {
+          expect(body).toEqual('response from lambda transformed from api gateway proxy response stream');
+          done();
+        });
+      });
+
+      it('can obsereve transformed request to lambda using subscription callback', (done) => {
+        let forkedRequest = '';
+        const observer = new Writable({
+          write(chunk, _, cb) {
+            forkedRequest += chunk.toString();
+            cb();
+          },
+        });
+
+        route.proxy(req, res, (_req) => {
+          _req.pipe(observer);
+        });
+
+        res.on('close', () => {
+          expect(forkedRequest).toEqual('request from local transformed from api gateway proxy request stream');
+          done();
+        });
+      });
+
+      it('can obsereve raw response from lambda using subscription callback', (done) => {
+        let forkedResponse = '';
+        const observer = new Writable({
+          write(chunk, _, cb) {
+            forkedResponse += chunk.toString();
+            cb();
+          },
+        });
+
+        route.proxy(req, res, (_, _res) => {
+          _res.pipe(observer);
+        });
+
+        res.on('close', () => {
+          expect(forkedResponse).toEqual('response from lambda');
+          done();
+        });
+      });
     });
   });
 });
